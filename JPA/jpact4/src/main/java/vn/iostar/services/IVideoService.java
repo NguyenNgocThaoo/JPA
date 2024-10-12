afbc8016160c3d4c81e1584bb2f51e16c24ddb20
NguyenNgocThaoo <137972394+NguyenNgocThaoo@users.noreply.github.com>
2024-10-12 17:37:31 +0700

    Add files via upload

---
+++ JPA/jpact4/src/main/java/vn/iostar/services/IVideoService.java
@@ -0,0 +1,22 @@
+package vn.iostar.services;
+
+import java.util.List;
+
+import vn.iostar.entity.Video;
+public interface IVideoService {
+	void insert(Video video);
+
+    void update(Video video);
+
+    void delete(int videoId) throws Exception;
+
+    Video findById(String videoId);
+
+    List<Video> findAll();
+
+    List<Video> findByVideoTitle(String title);
+
+    List<Video> findAll(int page, int pageSize);
+
+    int count();
+}
---
+++ JPA/jpact4/src/main/java/vn/iostar/ultis/Constant.java
@@ -0,0 +1,9 @@
+package vn.iostar.ultis;
+
+public class Constant {
+	public static final String SESSION_USERNAME = "username";
+	public static final String COOKIE_REMEMBER = "username";
+	 public static final String REGISTER = "/WEB-INF/views/register.jsp";
+	 public static final String UPLOAD_DIR = "D:\\uploads";
+	 public static final String DEFAULT_FILENAME = "default.file";
+}
